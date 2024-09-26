81d0e62b537e123e222e832b9890594397b3ed61
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-09-26 20:20:28 +0200

    feat: work queues example (WIP)

---
+++ example/code/work_queues.py
@@ -0,0 +1,39 @@
+from threading import Thread
+
+import pika
+
+
+def hello_world() -> None:
+    # We need to start a new thread to receive messages
+    # because channel.start_consuming() is blocking
+    thread1 = Thread(target=receive)
+    thread1.start()
+
+    for name in ["Alice", "Bob", "Katherine", "Peter", "Michael"]:
+        send(name)
+
+
+def send(name: str) -> None:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+    channel = connection.channel()
+    channel.queue_declare(queue="hello")
+    channel.basic_publish(exchange="", routing_key="hello", body=f"Hello {name}!")
+    print(f" [x] Sent 'Hello {name}!'")
+    connection.close()
+
+
+def receive() -> None:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+    channel = connection.channel()
+    channel.queue_declare(queue="hello")
+    channel.basic_consume(queue="hello", on_message_callback=_callback, auto_ack=True)
+
+    print(" [*] Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+
+
+def _callback(ch, method, properties, body):
+    print(f" [x] Received {body}")
+
+
+hello_world()
