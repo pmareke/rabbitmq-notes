059267fb4f2023063f9b4573f02424c3f3a81f91
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-09-27 12:47:29 +0200

    feat: add real example (WIP)

--- examples/real_example/server/main.py
+++ examples/real_example/server/main.py
@@ -1,19 +1,57 @@
+import json
+
 import pika
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/{name}")
-def read_root(name: str):
-    send(name)
-    return {"Hello": "World"}
+class User(BaseModel):
+    name: str
 
 
-def send(name: str) -> None:
+users: dict[str, User] = {}
+
+
+@app.get("/api/v1/users")
+def get_users() -> list[User]:
+    send("list_users")
+    return [user for user in users.values()]
+
+
+@app.get("/api/v1/users/{name}")
+def get_user(name: str) -> User:
+    send("get_user", name)
+    return users.get(name)
+
+
+@app.post("/api/v1/users/{name}")
+def add_user(name: str) -> User:
+    user = User(name=name)
+    users[name] = user
+    send("add_user", name)
+    return user
+
+
+@app.delete("/api/v1/users/{name}")
+def delete_user(name: str) -> User:
+    user = users[name]
+    del users[name]
+    send("delete_user", name)
+    return user
+
+
+def send(event_name: str, name: str | None = None) -> None:
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
     channel = connection.channel()
     channel.queue_declare(queue="hello")
-    channel.basic_publish(exchange="", routing_key="hello", body=f"Hello {name}!")
-    print(f" [x] Sent 'Hello {name}!'")
+
+    if name:
+        payload = json.dumps({"event": event_name, "name": name})
+    else:
+        payload = json.dumps({"event": event_name})
+
+    channel.basic_publish(exchange="", routing_key="hello", body=payload)
+    print(f" [x] Sent '{payload}'")
     connection.close()
